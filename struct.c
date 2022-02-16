6d047832f85eb80564bed6a99a35684c3066a805
kusuma <kusuma.g@thundersoft.com>
2022-02-16 21:30:03 +0530

    tructure coding

---
+++ struct.c
@@ -0,0 +1,12 @@
+void main()
+{
+ struct employee{
+  int age;
+  char name[10];
+  float salary;
+   }e1;
+ e1.age=20;
+ e1.name=‘mahi’;
+ e1.salary=4.5;
+ printf(“%d %c %f”,e1.age,e1.salary,e1.name);
+}
