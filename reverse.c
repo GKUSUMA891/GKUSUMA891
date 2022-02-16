7e1e5ae9d37c94277238a561df46301ddd5f0730
kusuma <kusuma.g@thundersoft.com>
2022-02-16 20:24:26 +0530

    reversing a program s Please enter the commit message for your changes. Lines starting

---
+++ reverse.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+char a[]=“thundersoft”;
+char temp;
+int i=0,j=0;
+while(a[j]!=‘\0’);
+j++;
+J=j-1;
+while(i<j)
+{
+temp=a[i];
+a[i]=a[j];
+a[j]=temp;
+i++;
+J--;
+Printf(“ reverse string is %s “,a);
+}
+
