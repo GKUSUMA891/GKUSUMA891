c055f305f1651384ad24999bbbe7900360958c22
kusuma <kusuma.g@thundersoft.com>
2022-02-23 12:17:58 +0530

    structure padding program

---
+++ structurepad.c
@@ -0,0 +1,14 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+struct abc
+{
+    int a;
+    char b;
+    short int c;
+    char d;
+}abc;
+void main()
+{
+    printf("%d",sizeof(struct abc));
+}
+
