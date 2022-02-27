8dc29d56b02cbe0cc61d1eb49922d2ae89196ebf
kusuma <kusuma.g@thundersoft.com>
2022-02-27 20:23:09 +0530

    WAP without using operator

---
+++ operator.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+  int n1=20,n2=30;
+  int i;
+  for(i=0;i<n2;i++)
+  {
+      n1++;
+  }
+  printf("sum of two numbers %d",n1);
+  getch();
+  return 0;
+  
+}
